5da8dfb508d6fb422db2b7e0673952573cdbbe69
vv4t <watashidesu6969@gmail.com>
2025-03-20 01:49:15 +1100

    basic map rnederer

--- src/renderer.js
+++ src/renderer.js
@@ -1,30 +1,20 @@
 "use strict"
 
 import { gl } from "./display.js";
-import { vertex_t } from "./vertex.js";
 import { camera_t } from "./camera.js";
 import { texture_t } from "./texture.js";
 import { get_asset } from "./assets.js";
 import { mesh_buffer_t } from "./mesh.js";
-import { vec2_t, vec3_t, mat4_t } from "./math.js";
+import { map_renderer_t } from "./map_renderer.js";
 import { world_shader_t } from "./world_shader.js";
+import { vec2_t, vec3_t, mat4_t } from "./math.js";
 
 export class renderer_t {
   constructor() {
-    this.mesh_buffer = new mesh_buffer_t(4096);
+    this.mesh_buffer = new mesh_buffer_t(16 * 1024);
     this.camera = new camera_t();
     this.world_shader = new world_shader_t();
-    
-    const vertices = [
-      new vertex_t(new vec3_t(+0.5, +0.5, 0.0), new vec2_t(1,1)),
-      new vertex_t(new vec3_t(+0.5, -0.5, 0.0), new vec2_t(1,0)),
-      new vertex_t(new vec3_t(-0.5, +0.5, 0.0), new vec2_t(0,1)),
-      
-      new vertex_t(new vec3_t(-0.5, -0.5, 0.0), new vec2_t(0,0)),
-      new vertex_t(new vec3_t(-0.5, +0.5, 0.0), new vec2_t(0,1)),
-      new vertex_t(new vec3_t(+0.5, -0.5, 0.0), new vec2_t(1,0))
-    ];
-    this.mesh = this.mesh_buffer.push(vertices);
+    this.map_renderer = new map_renderer_t(this.mesh_buffer);
     this.texture = new texture_t(get_asset("assets/test.png"));
     
     this.world_shader.bind();
@@ -36,9 +26,8 @@ export class renderer_t {
   
   render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    const model = mat4_t.init_translation(new vec3_t(0.5, 0.0, 0.0));
     this.camera.update_view();
-    this.world_shader.set_mvp(this.camera.get_mvp(model));
-    this.mesh.draw();
+    this.world_shader.set_mvp(this.camera.get_mvp(mat4_t.init_identity()));
+    this.map_renderer.render();
   }
 };
