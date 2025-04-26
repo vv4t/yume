5ce89ab3d6da5196f191132645e2b2a243f63175
vv4t <watashidesu6969@gmail.com>
2025-04-27 05:08:57 +1000

    cool

--- src/core/main.js
+++ src/core/main.js
@@ -4,7 +4,7 @@ import { map_t } from "./map.js";
 import { game_t } from "./game.js";
 import { input_t, input_axis } from "./input.js";
 import { bus_t } from "./bus.js";
-import { play_text } from "./text.js";
+import { play_conversation} from "./text.js";
 import { load_assets } from "./assets.js";
 import { renderer_t } from "../renderer/renderer.js";
 
@@ -19,12 +19,16 @@ function run() {
   input.bind_key_to_axis("a", input_axis.LEFT);
   input.bind_key_to_axis("d", input_axis.RIGHT);
 
-  bus.raise_events(["load_map_largechamber"]);
+  bus.raise_events(["load_map_desert"]);
+
+  let tick = 0;
 
   function update() {
-    process_bus();
-    game.update();
-    renderer.render();
+    if (tick++ % 2 === 0) {
+      process_bus();
+      game.update();
+      renderer.render();
+    }
     requestAnimationFrame(update);
   }
 
@@ -48,4 +52,4 @@ function run() {
 
 load_assets().then(() => run());
 
-play_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+setTimeout(() => play_conversation("SWARM"), 200);
--- src/core/text.js
+++ src/core/text.js
@@ -1,11 +1,76 @@
-const textArea = document.getElementById("text");
+"use strict";
 
-export function play_text(text) {
-  console.log(text);
-  const DELAY = 5;
+import { get_asset } from "../core/assets.js";
+
+let history = "";
+
+const textArea = document.getElementById("text");
+textArea.value = history;
+
+export function play_conversation(name) {
+  const text = get_asset(`assets/data/${name}.txt`);
+  submit_text(text);
+}
+
+export function submit_text(text) {
+  history += text;
+  play_text(text, 10);
+}
+
+function show_log(entry) {
+  if (entry > 0) {
+    clear_text();
+    play_text(get_asset(`assets/data/LOG${entry}.txt`), 5);
+  } else {
+    textArea.value = history;
+  }
+}
+
+function clear_text() {
+  textArea.value = "";
+}
+
+function get_log_buttons() {
+  const log_buttons = [document.getElementById("log_main")];
+
+  for (let i = 1; true; i++) {
+    const log_button = document.getElementById(`log_${i}`);
+    if (!log_button) break;
+    log_buttons.push(log_button);
+  }
+
+  return log_buttons;
+}
+
+function lock_text() {
+  get_log_buttons().forEach((button) => {
+    button.style.color = "#559955";
+    button.disabled = true;
+  });
+}
+
+function unlock_text() {
+  get_log_buttons().forEach((button) => {
+    button.style.color = "#aaffaa";
+    button.disabled = false;
+  });
+}
+
+function play_text(text, delay) {
+  lock_text();
+
+  text = text.split("\n\n").map((chunk) => chunk.replace(/\n/g, " ")).join("\n\n");
+  
   for (let i = 0; i < text.length; i++) {
     setTimeout(() => {
       textArea.value += text[i];
-    }, (i + 1) * DELAY);
+      textArea.scrollTop = textArea.scrollHeight;
+    }, (i + 1) * delay);
   }
+
+  setTimeout(() => unlock_text(), (text.length + 1) * delay);
 }
+
+get_log_buttons().forEach((button, i) => {
+  button.addEventListener("click", () => show_log(i));
+});
